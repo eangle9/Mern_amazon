acf7b3a9d410f775f01c71e87e91b419f1138fe7
eangle9 <engdaworkyismaw9@gmail.com>
2024-02-01 19:22:04 +0300

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
