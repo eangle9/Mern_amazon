3cb5e69d70cc04125a2a161d47ed3cd3fb38a888
eangle9 <engdaworkyismaw9@gmail.com>
2024-02-02 19:48:53 +0300

    backend for detail product page

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,27 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 // app.use(dotenv());
 
 app.get("/", (req, res) => {
-    res.send("api is running...")
-})
+  res.send("api is running...");
+});
 app.get("/api/products", (req, res) => {
   res.send(data.products);
+});
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find(
+    (product) => product.slug === req.params.slug
+  );
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).json({ message: "Product Not Found" });
+  }
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`);
-})
+  console.log(`serve at http://localhost:${port}`);
+});
