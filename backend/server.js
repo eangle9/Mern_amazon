ff82c8768fb911ab09b9e58e87a77438de048a79
eangle9 <engdaworkyismaw9@gmail.com>
2024-02-03 23:18:56 +0300

    chore: single product return at the backend

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,16 @@ app.get("/api/products/slug/:slug", (req, res) => {
     res.status(404).json({ message: "Product Not Found" });
   }
 });
+app.get("/api/product/:id", (req, res) => {
+  const product = data.products.find(
+    (product) => product._id === req.params.id
+  );
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).json({ message: "Product Not Found" });
+  }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
